09c315f3fead22394d6ca25e8390d33393c23bbc
Yash Bansal <ybansalnaveen@gmail.com>
2021-06-28 23:04:58 +0530

    First

---
+++ app/src/main/java/com/myfirst/viewsandlayout/MainActivity.java
@@ -0,0 +1,17 @@
+package com.myfirst.viewsandlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
